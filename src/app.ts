9c96d258ba4f0776e1e2661ffb102048c3b324a5
Liangzhijun123 <Liangzhijun1216@gmail.com>
2026-04-05 12:52:44 -0400

    Fix count endpoint, invalid route handling, and CORS issues

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,18 @@ import loggerService from './services/logger.service';
 
 const app = express();
 app.disable("x-powered-by")
-app.use(cors())
-app.use(cors({ credentials: true, origin: '*' }));
+app.use(cors({
+    origin: '*',
+    credentials: true
+}));
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+});
+
+app.get('/favicon.ico', (req, res) => {
+    res.status(204).end(); // no content
+});
 
 app.locals.HEALTH_CHECK_ENABLED = true;
 app.get("/health", (_, res) => {
@@ -21,18 +31,26 @@ app.get("/health", (_, res) => {
 })
 
 app.use(express.json());
-app.use('*', router);
+app.use('/', router);
 
-app.use(async (req, res: Response, next: NextFunction) => {
-    next(createHttpError.BadRequest());
+app.use((req: Request, res: Response) => {
+    res.status(400).send({
+        error: {
+            status: 400,
+            message: "Invalid Request"
+        }
+    });
 });
 
-app.use(async (err: any, req: Request, res: Response, next: NextFunction) => {
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     loggerService.error({ message: err.message, path: req.path }).flush();
-    res.status(500).send({
+
+    const status = err.status || 500;
+
+    res.status(status).send({
         error: {
-            status: 500,
-            message: "Internal Error",
+            status,
+            message: err.message || "Internal Error",
         }
     });
 });
--- src/endpoints/base.endpoint.ts
+++ src/endpoints/base.endpoint.ts
@@ -1,44 +1,106 @@
-import { NextFunction, Request, Response } from 'express';
-import responseWrapper from '../services/response.service';
-import { RESPONSE_STATUS_FAIL, RESPONSE_EVENT_READ } from '../constants/generic.constants';
-import { INVALID_REQUEST } from '../constants/errors.constants';
-import createHttpError from 'http-errors';
+import { NextFunction, Request, Response } from "express";
+import responseWrapper from "../services/response.service";
+import {
+  RESPONSE_STATUS_FAIL,
+  RESPONSE_EVENT_READ,
+} from "../constants/generic.constants";
+import { INVALID_REQUEST } from "../constants/errors.constants";
+import createHttpError from "http-errors";
 
 class BaseEndpoint {
-    private readonly extensions = new Map<string, string>([
-        ["dev", ".js"],
-        ["prod", ".ts"]
-    ]);
+  private readonly extensions = new Map<string, string>([
+    ["dev", ".js"],
+    ["prod", ".ts"],
+  ]);
 
-    public constructor() { }
+  public constructor() {}
 
-    public get(req: Request, res: Response, next: NextFunction) {
-        throw new createHttpError.BadRequest();
+  public get(req: Request, res: Response, next: NextFunction) {
+    return res
+      .status(400)
+      .send(
+        responseWrapper(
+          RESPONSE_STATUS_FAIL,
+          RESPONSE_EVENT_READ,
+          INVALID_REQUEST,
+        ),
+      );
+  }
+
+  public post(req: Request, res: Response, next: NextFunction) {
+    return res
+      .status(400)
+      .send(
+        responseWrapper(
+          RESPONSE_STATUS_FAIL,
+          RESPONSE_EVENT_READ,
+          INVALID_REQUEST,
+        ),
+      );
+  }
+
+  public put(req: Request, res: Response, next: NextFunction) {
+    return res
+      .status(400)
+      .send(
+        responseWrapper(
+          RESPONSE_STATUS_FAIL,
+          RESPONSE_EVENT_READ,
+          INVALID_REQUEST,
+        ),
+      );
+  }
+
+  public delete(req: Request, res: Response, next: NextFunction) {
+    return res
+      .status(400)
+      .send(
+        responseWrapper(
+          RESPONSE_STATUS_FAIL,
+          RESPONSE_EVENT_READ,
+          INVALID_REQUEST,
+        ),
+      );
+  }
+
+  public executeSubRoute(
+    endPointMethod: any,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    let subRoute = req.originalUrl.split("/")[2];
+
+    if (!subRoute) {
+      return res
+        .status(400)
+        .send(
+          responseWrapper(
+            RESPONSE_STATUS_FAIL,
+            RESPONSE_EVENT_READ,
+            INVALID_REQUEST,
+          ),
+        );
     }
 
-    public post(req: Request, res: Response, next: NextFunction) {
-        throw new createHttpError.BadRequest();
+    subRoute = `${subRoute}_${req.method.toLowerCase()}`;
+
+    const temp = endPointMethod[subRoute as keyof typeof endPointMethod];
+
+    if (!temp) {
+      return res
+        .status(400)
+        .send(
+          responseWrapper(
+            RESPONSE_STATUS_FAIL,
+            RESPONSE_EVENT_READ,
+            INVALID_REQUEST,
+          ),
+        );
     }
 
-    public put(req: Request, res: Response, next: NextFunction) {
-        throw new createHttpError.BadRequest();
-    }
-
-    public delete(req: Request, res: Response, next: NextFunction) {
-        throw new createHttpError.BadRequest();
-    }
-
-    public executeSubRoute(endPointMethod: any, req: Request, res: Response, next: NextFunction) {
-        let subRoute = req.originalUrl.split('/')[2];
-        subRoute = `${subRoute}_${req.method.toLowerCase()}`
-
-        const temp = endPointMethod[subRoute as keyof typeof endPointMethod];
-        if (!temp) {
-            throw new createHttpError.BadRequest();
-        }
-
-        temp(req, res, next);
-    }
+    temp.call(endPointMethod, req, res, next);
+  }
 }
 
 export default BaseEndpoint;
